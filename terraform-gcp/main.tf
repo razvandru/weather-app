d8b400edb0bc6f1a0bc0348e4c3bdb0accbf2fd1
razva <razvan.drumesi@gmail.com>
2026-04-17 00:03:41 +0300

    Change default node

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -11,25 +11,21 @@ provider "google" {
 }
 
 resource "google_container_cluster" "primary" {
-  name     = "${var.cluster_name}-gke"
-  location = var.gcp_region
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  deletion_protection = false # Explicitly disable deletion protection
-}
+  name                = "${var.cluster_name}-gke"
+  location            = var.gcp_region
+  deletion_protection = false
 
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${var.cluster_name}-node-pool"
-  location   = var.gcp_region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.node_count
+  node_pool {
+    name       = "${var.cluster_name}-node-pool"
+    node_count = var.node_count
 
-  node_config {
-    preemptible  = false
-    machine_type = "e2-medium"
-    disk_type    = "pd-standard"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    node_config {
+      preemptible  = false
+      machine_type = "e2-medium"
+      disk_type    = "pd-standard"
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform"
+      ]
+    }
   }
 }
